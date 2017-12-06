3e39a0682e618915b4b0ad357083598dbefe441b
LCShao <2859594131@qq.com>
2017-12-06 15:47:41 +0800

    20171206

--- data/beauty.sql
+++ data/beauty.sql
@@ -850,11 +850,11 @@ CREATE TABLE `safeshopping` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `safeshopping` (`sid`, `img`, `title`, `details`) VALUES
-(1, 'img/safeShopping/20170515142054653.png', '美白', '白嫩无暇遮百丑'),
-(2, 'img/safeShopping/20170515142103595.png', '面部提拉', '对抗时间重塑V脸'),
-(3, 'img/safeShopping/20170420220545236.png', '祛痘', '最重要的是“无暇”'),
-(4, 'img/safeShopping/20170515142118861.png', '瘦身', '瘦才能百搭'),
-(5, 'img/safeShopping/20170515142111451.png', '除皱', '是时候抗老啦');
+(1, 'img/safe-shopping/20170515142054653.png', '美白', '白嫩无暇遮百丑'),
+(2, 'img/safe-shopping/20170515142103595.png', '面部提拉', '对抗时间重塑V脸'),
+(3, 'img/safe-shopping/20170420220545236.png', '祛痘', '最重要的是“无暇”'),
+(4, 'img/safe-shopping/20170515142118861.png', '瘦身', '瘦才能百搭'),
+(5, 'img/safe-shopping/20170515142111451.png', '除皱', '是时候抗老啦');
 
 drop table if exists safeshoppingsecond;
 CREATE TABLE `safeshoppingsecond` (
@@ -865,12 +865,12 @@ CREATE TABLE `safeshoppingsecond` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `safeshoppingsecond` (sid, `img`, `title`, `details`) VALUES
-(1,'img/safeShopping/20170420160118770.png', '白富美必备', '高端定制'),
-(2,'img/safeShopping/20170420160208371.png', '学生白领馆', '变美趁早'),
-(3,'img/safeShopping/20170420160237237.png', '专家医生馆', '安心靠谱'),
-(4,'img/safeShopping/20170420160311926.png', '日记返巨款', '变美返现'),
-(5,'img/safeShopping/20170420160337527.png', '韩日泰港台', '海外整形'),
-(6,'img/safeShopping/20170420160359622.png', '瘦才是王道', '快速瘦身'),
-(7,'img/safeShopping/20170420160424753.png', '初恋少女馆', '脂肪填充'),
-(8,'img/safeShopping/20170515142622230.png', '换季护肤馆', '皮肤管理');
+(1,'img/safe-shopping/20170420160118770.png', '白富美必备', '高端定制'),
+(2,'img/safe-shopping/20170420160208371.png', '学生白领馆', '变美趁早'),
+(3,'img/safe-shopping/20170420160237237.png', '专家医生馆', '安心靠谱'),
+(4,'img/safe-shopping/20170420160311926.png', '日记返巨款', '变美返现'),
+(5,'img/safe-shopping/20170420160337527.png', '韩日泰港台', '海外整形'),
+(6,'img/safe-shopping/20170420160359622.png', '瘦才是王道', '快速瘦身'),
+(7,'img/safe-shopping/20170420160424753.png', '初恋少女馆', '脂肪填充'),
+(8,'img/safe-shopping/20170515142622230.png', '换季护肤馆', '皮肤管理');
 
