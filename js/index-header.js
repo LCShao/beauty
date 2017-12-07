c8751fa9b6e4f55bf3f7203c5f26dbbff6a4e1a8
LCShao <2859594131@qq.com>
2017-12-07 23:07:57 +0800

    20171207

--- js/index-header.js
+++ js/index-header.js
@@ -10,14 +10,22 @@ $(()=>{
   }
   function loadSHelper(){
     var $input=$("#sbox>input"),
-        $shelper=$input.next();
+        $shelper=$input.prev();
     if($input.val().trim().length) {
       $shelper.show();
       $.get("data/index/searchHelper.php",{
         kw:$input.val().trim()
       }).then(data=>{
-        console.log(data);
+        var html="";
+        for(var d of data){
+          html+=`<li data-product_id="${d.product_id}" title="${d.kword+d.title}">
+            <div class="search-item" title="${d.kword+d.title}"><span>${d.kword+d.title}</span><b>预约: ${d.yy_count}</b></div>
+          </li>`;
+        }
+        $shelper.html(html);
       })
+    }else{
+      $shelper.hide();
     }
 
   }
@@ -37,8 +45,43 @@ $(()=>{
     }).blur(e=>{
       $(e.target).prev().hide();
     }).keyup(e=>{
-      loadSHelper();
-    })
+      var $shelper=$(e.target).prev();
+      switch(e.keyCode){
+        case 38:
+          if(!$shelper.is(":has(.hover)")){
+            $shelper.children().last().addClass("hover");
+          }else if($shelper.children().first().hasClass("hover")){
+            $shelper.children().first().removeClass("hover");
+            $shelper.children().last().addClass("hover");
+          }else{
+            $shelper.children(".hover").removeClass("hover")
+              .prev().addClass("hover");
+          }
+          break;
+        case 40:
+          if(!$shelper.is(":has(.hover)")){
+            $shelper.children().first().addClass("hover");
+          }else if($shelper.children().last().hasClass("hover")){
+            $shelper.children().last().removeClass("hover");
+            $shelper.children().first().addClass("hover");
+          }else{
+            $shelper.children(".hover").removeClass("hover")
+              .next().addClass("hover");
+          }
+          break;
+        case 13:
+          if($shelper.is(":has(.hover)")){
+            var $hover=$shelper.children(".hover");
+            location="product-details.html?pid="+$hover.data("product_id");
+          }else{
+            location="safe-shopping.html?kw="+$(e.target).val();
+          }
+          break;
+        default:
+          loadSHelper();
+      }
+
+    });
   });
   $("#footer").load("index-footer.html");
 });
